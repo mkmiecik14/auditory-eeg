a3e60371377ac46c7a9bd76b599e7bde42c4f481
Matt Kmiecik <mkmiecik14@gmail.com>
2021-01-19 16:59:55 -0600

    created r-workspace-prep.R and behav prepro scripts

---
+++ auditory-behav-prepro.R
@@ -0,0 +1,48 @@
+# Auditory behavioral task preprocessing script
+# This script takes the merged e-data aid file and preprocesses it
+# for analysis in R
+#
+# Matt Kmiecik
+# Started 19 JAN 2021
+
+# Prepares workspace ----
+source("r-workspace-prep.R")
+
+# Loads data and processes----
+behav_data <- 
+  read_delim(
+    file = "../data/eprime/auditory-behav-data.txt", 
+    delim = "\t"
+    ) %>%
+  # selects relevant variables
+  select(
+    ss = Subject,
+    date = SessionDate,
+    order = Block,
+    intensity = BlockList,
+    rating_0 = RateSc.RESP,
+    rt_0 = RateSc.RT,
+    rating_1 = RateSc1.RESP,
+    rt_1 = RateSc1.RT,
+    rating_2 = RateSc2.RESP,
+    rt_2 = RateSc2.RT,
+    rating_3 = RateSc3.RESP,
+    rt_3 = RateSc3.RT,
+    rating_4 = RateSc4.RESP,
+    rt_4 = RateSc4.RT
+  ) %>%
+  mutate(across(starts_with("rating"), as.numeric)) %>% # changes ratings to num
+  pivot_longer(
+    cols = starts_with(c("rating","rt")),
+  ) %>%
+  filter(complete.cases(value)) %>% # gets rid of missing values
+  separate(name, into = c("meas", "delete")) %>% # simplifies
+  select(-delete) %>% # gets rid of useless column
+  arrange(ss, intensity) # arranges for aesthetics
+
+# Saves out cleaned and processed data ----
+save(behav_data, file = "../output/behav-data.RData")
+
+# Cleans workspace ----
+rm(behav_data)
+
---
+++ r-workspace-prep.R
@@ -0,0 +1,14 @@
+# Auditory Project R Workspace Prep
+# This script prepares the R workspace for the auditory project
+# by loading packages, color palettes, global variables, etc.
+#
+# Matt Kmiecik
+# Started 19 JAN 2021
+
+# Load libraries ----
+print("Loading R packages...")
+library(tidyverse)
+library(broom)
+library(Hmisc)
+library(RColorBrewer)
+library(readxl)
